ff054a12764d2e9a024a3599dd56f49d09bdaea6
EDA <eda@mac-mini-3.local>
2017-03-17 11:53:54 +1300

    form structure created

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Add from './Add'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <h1>React development has begun!</h1>
+      <Add />
+    </div>
   )
 }
 
 export default App
-
