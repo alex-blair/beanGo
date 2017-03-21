98f337ab65b499fe93039fa227c6b677b14e4204
Tony Luisi <t.luisi@gmail.com>
2017-03-21 15:26:42 +1300

    removed db fake data

--- db/db.js
+++ db/db.js
@@ -6,35 +6,6 @@ module.exports = {
   getCoffeeList: getCoffeeList,
 }
 
-var coffeeList = [{
-  id: 1,
-  name: 'Mocha'
-},
-{
-  id: 2,
-  name: 'Flat White'
-},
-{
-  id: 3,
-  name: 'Short Black'
-},
-{
-  id: 4,
-  name: 'Long Black'
-},
-{
-  id: 5,
-  name: 'Americano'
-},
-{
-  id: 6,
-  name: 'Green Tea'
-},
-{
-  id: 7,
-  name: 'Earl Grey'
-}]
-
 function getCoffeeList () {
   return knex('coffees')
 }
