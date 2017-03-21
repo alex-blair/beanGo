c875a68fbf1ca1d5ff6ff33a9c6d8f765a524a25
Tony Luisi <t.luisi@gmail.com>
2017-03-21 13:59:13 +1300

    added routes to server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,17 @@ var bodyParser = require('body-parser')
 
 var server = express()
 
+var coffees = require('./routes/coffees')
+var orders = require('./routes/orders')
+
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
+
+server.use('/coffees', coffees)
+server.use('/orders', orders)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
 
 module.exports = server
